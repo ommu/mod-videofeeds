f4c2bc059e975a6ddbf8933c87b00d7961768839
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-31 12:29:59 +0700

    change author email

--- modules/video/views/o/likes/admin_manage.php
+++ modules/video/views/o/likes/admin_manage.php
@@ -4,7 +4,7 @@
  * @var $this LikesController
  * @var $model VideoLikes
  *
- * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
  * @link https://github.com/oMMu/Ommu-Video-Albums
  * @contect (+62)856-299-4114
