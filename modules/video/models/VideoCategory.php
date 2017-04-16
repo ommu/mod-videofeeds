f622385abbdce6be0b75c9e20509de7aec5eb78c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-16 20:41:04 +0700

    model: commit

--- modules/video/models/VideoCategory.php
+++ modules/video/models/VideoCategory.php
@@ -281,7 +281,7 @@ class VideoCategory extends CActiveRecord
 					'model'=>$this,
 					'attribute'=>'creation_date',
 					'language' => 'en',
-					'i18nScriptFile' => 'jquery-ui-i18n.min.js'
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
 					//'mode'=>'datetime',
 					'htmlOptions' => array(
 						'id' => 'creation_date_filter',
--- modules/video/models/VideoLikes.php
+++ modules/video/models/VideoLikes.php
@@ -223,7 +223,7 @@ class VideoLikes extends CActiveRecord
 					'model'=>$this,
 					'attribute'=>'likes_date',
 					'language' => 'en',
-					'i18nScriptFile' => 'jquery-ui-i18n.min.js'
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
 					//'mode'=>'datetime',
 					'htmlOptions' => array(
 						'id' => 'likes_date_filter',
