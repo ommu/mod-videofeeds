d7ee7a353eb83d7b18960abf26316e4d3a90119d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-03-29 11:56:28 +0700

    commit

--- views/o/admin/admin_headline.php
+++ views/o/admin/admin_headline.php
@@ -25,7 +25,7 @@
 )); ?>
 
 	<div class="dialog-content">
-		<?php echo Yii::t('phrase', 'Are you sure you want to headline this item?');?>	</div>
+		<?php echo Yii::t('phrase', 'Are you sure you want to headline this item?');?></div>
 	<div class="dialog-submit">
 		<?php echo CHtml::submitButton(Yii::t('phrase', 'Headline'), array('onclick' => 'setEnableSave()')); ?>
 		<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
