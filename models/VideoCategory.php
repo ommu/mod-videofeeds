7c5c56f83ff50a8278e9393e02da4fce9411985e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-06-28 14:16:12 +0700

    add count column condition in getInfo function

--- models/VideoCategory.php
+++ models/VideoCategory.php
@@ -350,9 +350,12 @@ class VideoCategory extends CActiveRecord
 	{
 		if($column != null) {
 			$model = self::model()->findByPk($id,array(
-				'select' => $column
+				'select' => $column,
 			));
-			return $model->$column;
+			if(count(explode(',', $column)) == 1)
+				return $model->$column;
+			else
+				return $model;
 
 		} else {
 			$model = self::model()->findByPk($id);
