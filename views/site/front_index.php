e840ddf5b25101cc8031113229f674361e41290c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-09 11:22:38 +0700

    commit

--- views/site/front_index.php
+++ views/site/front_index.php
@@ -18,7 +18,7 @@
 ?>
 
 <div class="box list">
-	<?php $this->widget('application.libraries.core.components.system.FListView', array(
+	<?php $this->widget('zii.widgets.CListView', array(
 		'dataProvider'=>$dataProvider,
 		'itemView'=>'_view',
 		'pager' => array(
